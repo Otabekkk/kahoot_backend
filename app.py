dd84cdf2035115b91f3edd02d9c0748a3749ecae
Otabekkk <otabekiusupov1@gmail.com>
2025-03-28 14:46:38 +0600

    Register Log in

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import secrets
 from flasgger import Swagger
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 # from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 Swagger(app)
 CORS(app)
+jwt = JWTManager(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = ''
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -15,12 +18,58 @@ app.config['SESSION_COOKIE_SECURE'] = False
 app.secret_key = secrets.token_hex(16)
 
 
-
 from models import Admin, Test, db
 db.init_app(app)
 with app.app_context():
     db.create_all()
 # migrate = Migrate(app, db)
+
+
+@app.route('/register', methods = ['POST'])
+def register():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    if Admin.query.filter_by(username = username).first():
+        return jsonify({'Error': 'Username already exists'})
+    
+    admin = Admin(username = username, password = password)
+    db.session.add(admin)
+    db.session.commit()
+
+    return jsonify({'Success': 'Admin created'})
+
+
+@app.route('/login', methods = ['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    admin = Admin.query.filter_by(username = username, password = password).first()
+
+    if not admin:
+        return jsonify({'Error': 'Invalid data'})
+    
+    access_token = create_access_token(identity = admin.id)
+
+    return jsonify(access_token = access_token)
+
+
+@app.route('/tests', methods = ['POST'])
+@jwt_required()
+def create_test():
+    user_id = get_jwt_identity()
+
+    test = Test(user_id = user_id)
+
+    db.session.add(test)
+    db.session.commit()
+
+    return jsonify({'Success': 'Test created!', 'Test id': test.id})
+    
+
 
 @app.route('/admins/<int:user_id>', methods = ['GET'])
 def get_admin(user_id: int):
@@ -48,6 +97,7 @@ def get_admin(user_id: int):
       404:
         description: Администратор не найден
     """
+
     admin = Admin.query.get(user_id)
     if not admin:
         return jsonify({'Error': 'User not found'}), 404
@@ -105,6 +155,7 @@ def create_admin():
       500:
         description: Внутренняя ошибка сервера
     """
+
     data = request.get_json()
     try:
         admin = Admin(
