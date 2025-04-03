16b63e32a4be191d7b5e166c3a6762f296e63fd4
Otabekkk <otabekiusupov1@gmail.com>
2025-04-04 00:19:35 +0600

    models

--- backend/models.py
+++ backend/models.py
@@ -19,7 +19,9 @@ class Question(db.Model):
     option_4 = db.Column(db.String(255), nullable=False)
     correct_option = db.Column(db.Integer, nullable = False)
 
-class Player(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), nullable = False)
-    score = db.Column(db.Integer, default = 0)
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(50), nullable = False)
+    password = db.Column(db.String(50), nullable = False)
+
+    quizzes = db.relationship('Quiz', backref = 'admin', lazy = True)
